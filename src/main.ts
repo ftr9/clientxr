0ec480e88ebd0cb82d5f8c6a536c9d59b3c45ba0
ftr9 <ferlandodotel16@gmail.com>
2022-11-08 16:34:55 +0545

    added cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationError } from 'class-validator/types/validation/ValidationErro
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(
     new ValidationPipe({
       exceptionFactory: (validationErrors: ValidationError[]) => {
