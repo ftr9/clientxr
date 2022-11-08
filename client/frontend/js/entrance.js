20f8be792ae3c4ca06c4ed8297c4681fc0c62f05
ftr9 <ferlandodotel16@gmail.com>
2022-11-08 16:57:20 +0545

    letssee

--- client/frontend/js/entrance.js
+++ client/frontend/js/entrance.js
@@ -35,15 +35,11 @@ window.addEventListener('DOMContentLoaded', () => {
     button.textContent = 'uploading ...';
 
     try {
-      await axios.post(
-        'https://ncit-college.herokuapp.com/admission',
-        formDatas,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
+      await axios.post('/admission', formDatas, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
         },
-      );
+      });
       alert('form uploaded successfully ...');
       window.location.href = '/';
     } catch (err) {
